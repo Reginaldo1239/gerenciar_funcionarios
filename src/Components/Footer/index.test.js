cf4aad05262b744454ea4c8b0b46acf7d554745d
Reginaldo Melo <reginaldo.melo@rockapps.com.br>
2021-11-13 21:42:13 -0300

    test footer

---
+++ src/Components/Footer/index.test.js
@@ -0,0 +1,13 @@
+import { render, rerender, screen, fireEvent, } from '@testing-library/react';
+import faker from 'faker'
+import Styles from './index.module.css';
+import Footer from './index';
+
+test('render componente footer e verifica o texto', () => {
+  const title = faker.lorem.words();
+  const textComponentRight = faker.lorem.words();
+  const { container } = render(<Footer />);
+  expect(container.getElementsByClassName(Styles.footer)[0]).toHaveTextContent('todos os direitos reservados');
+
+});
+
