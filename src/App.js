7cf62d328c44a22bd68ad773da8b9bbced303452
Reginaldo Melo <reginaldo.melo@rockapps.com.br>
2021-11-14 11:44:38 -0300

    component ReduxProvider criado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
-
 import './App.css';
 import './Styles/Variables/index.css'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { store, persistor } from './Redux/Store';
+import ReduxProvider from './Components/ReduxProvider';
 import Loading from './Components/Loading/index';
 import Routes from './Routes'
 
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-          <Routes />
-          <Loading />
-        </div>
-      </PersistGate>
-    </Provider >
-
+    <ReduxProvider>
+      <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+        <Routes />
+        <Loading />
+      </div>
+    </ReduxProvider>
   );
 }
 
---
+++ src/Components/ReduxProvider/index.js
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from '../../Redux/Store';
+const ReduxProvider = (props) => {
+  const { children } = props;
+  return (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>)
+        {children}
+    </PersistGate>
+  </Provider >
+  )
+}
+export default ReduxProvider;
